9b289e578ee13bf22bd73a5e2e765b279ab272a0
姚成 <kixi@163.com>
2016-08-03 22:51:55 +0800

    优化删除冗余文件

--- src/index.js
+++ src/index.js
@@ -6,17 +6,6 @@
 
  'use strict';
 
-// import index from 'js/index';
-// import index from 'js/index/index2';
-// import index from 'js/index/index3';
-import index from 'js/index/index4';
-
-// import {simpleScrollTab, defaultScrollTab, iconScrollTab as index} from 'js/scrolltab';
-// import index from 'js/scrolltab/full';
-// import index from 'js/scrollview';
-// import index from 'js/listview';
-
-// import index from 'js/router';
-// import {AlertExample as index, SimpleAlertExampleBlock} from 'js/rndemo/AlertExample';    // .AlertExample   .SimpleAlertExampleBlock
+import index from 'js/index/router';
 
 export default index

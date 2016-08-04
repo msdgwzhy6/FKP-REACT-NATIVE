52dfa6062aadc63cf426554f1aef3cd86a5bedd9
姚成 <kixi@163.com>
2016-08-04 21:24:26 +0800

    新增后退图片

--- src/images/index.js
+++ src/images/index.js
@@ -2,6 +2,7 @@ export default {
     leftTile: require('./leftTile.png'),
     logo: require('./logo.png'),
     badge: require('./circle.png'),
+    arrowLeft: require('./arrow_left64.png'),
     inner: {
         facemash: require('./facemash.png'),
         messages: require('./messages.png'),
